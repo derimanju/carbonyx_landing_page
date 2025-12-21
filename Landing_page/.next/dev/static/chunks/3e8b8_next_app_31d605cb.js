8455c2c144560f83077522f5525d34f6335d7d51
kimminjuyo <kimmanju@manjumacproui-MacBookPro.local>
2025-12-21 22:50:08 +0900

    chore: remove residual directory

--- Landing_page/.next/dev/static/chunks/3e8b8_next_app_31d605cb.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/Desktop/Carbonyx_Landing_Page/node_modules/next/app.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/Desktop/Carbonyx_Landing_Page/node_modules/next/dist/pages/_app.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=3e8b8_next_app_31d605cb.js.map
--- Landing_page/.next/dev/static/chunks/3e8b8_next_error_b64413dc.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/Desktop/Carbonyx_Landing_Page/node_modules/next/error.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/Desktop/Carbonyx_Landing_Page/node_modules/next/dist/pages/_error.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=3e8b8_next_error_b64413dc.js.map
